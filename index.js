1ddadedbb0eae6e8ec970d329195bf3c7869fd35
Badmatrix <100013800+Badmatrix@users.noreply.github.com>
2023-09-28 13:28:05 +0100

    https update

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const icon = document.querySelector('.icon img')
 const timeStamp=document.querySelector('.utc')
 
 const getCity = async(city)=> {
-    const url = `http://dataservice.accuweather.com/locations/v1/cities/search`
+    const url = `https://dataservice.accuweather.com/locations/v1/cities/search`
     const query = `?apikey=${key}&q=${city}`
     const response = await fetch(url + query)
     const data = await response.json()
     return data[0]
 }
 const getWeather = async (id) => {
-    const url = `http://dataservice.accuweather.com/currentconditions/v1/${id}`
+    const url = `https://dataservice.accuweather.com/currentconditions/v1/${id}`
     const query = `?apikey=${key}`
     const response = await fetch(url + query)
     const data = await response.json()
@@ -22,7 +22,7 @@ const getWeather = async (id) => {
 }
 
 const getTime = async (location) => {
-    const base = 'http://worldtimeapi.org/api/timezone/'
+    const base = 'https://worldtimeapi.org/api/timezone/'
     const query=location
    const response= await fetch(base+location)
     const data = await response.json()
